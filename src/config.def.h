ec58dfc1fbe8a8936c7a7e22d782784ae3103aa3
sub-kek <subbkek@gmail.com>
2024-06-28 00:46:00 +0300

    Hz changex

--- src/config.def.h
+++ src/config.def.h
@@ -10,9 +10,9 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */                              
-static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 0;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -21,7 +21,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -63,12 +63,12 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *druncmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const Arg scrncmd = SHCMD("maim -b 4 -s $(xdg-user-dir PICTURES)/$(date +'%Y-%m-%d-%H%M%S.png')");
 
 static const Key keys[] = {
 	/* modifier                     key        function          argument */
@@ -76,16 +76,15 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_q,			 spawn,            {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,        {0} },
 	{ MODKEY,                       XK_f,      togglefullscreen, {0} },
-	{ MODKEY,                       XK_j,      focusstack,       {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,       {.i = -1 } },
+	{ MODKEY,                       XK_Tab,    focusstack,       {.i = +1 } },
 	{ MODKEY,                       XK_i,      incnmaster,       {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,       {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,         {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,         {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,             {0} },
-	{ MODKEY,                       XK_Tab,    view,             {0} },
 	{ MODKEY,                       XK_c,      killclient,       {0} },
 	{ MODKEY,                       XK_t,      togglefloating,   {0} },
+	{ 0,                            XK_Print,  spawn,						 scrncmd },
 	{ MODKEY,                       XK_0,      view,             {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,              {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,         {.i = -1 } },
